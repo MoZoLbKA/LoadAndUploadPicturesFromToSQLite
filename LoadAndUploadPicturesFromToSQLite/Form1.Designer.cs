0d1e212051f40cd4ca24b93840fd7372661e2415
Никита Макурин <makurin.nik@list.ru>
2022-03-30 12:12:35 +0300

    Добавьте файлы проекта.

---
+++ LoadAndUploadPicturesFromToSQLite/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace LoadAndUploadPicturesFromToSQLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(248, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 241);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(289, 326);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(202, 23);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Подгрузить в бд";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LoadAndUploadPicturesFromToSQLite/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LoadAndUploadPicturesFromToSQLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Image> images = new List<Image>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Array buf = null;
+            //метод подгружает в БД
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                MemoryStream ms = new MemoryStream();
+                pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                buf = ms.ToArray();
+            }
+            SQLiteConnection con = new SQLiteConnection("Data Source=pictures.db;Version=3;");
+            con.Open();
+            SQLiteCommand com = new SQLiteCommand("insert into images(images) values(@photo)", con);
+            com.Parameters.Add("@photo", DbType.Binary, 10000).Value = buf;
+            com.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //метод выгружает из дб и добавляет в лист картинок
+            SQLiteConnection con = new SQLiteConnection("Data Source=pictures.db;Version=3;");
+            SQLiteCommand cmd = new SQLiteCommand("SELECT images FROM images", con);
+            con.Open();
+            SQLiteDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (!dr.IsDBNull(0))
+                {
+                    byte[] im = (byte[])dr["images"];
+                    MemoryStream ms = new MemoryStream(im);
+                    
+                    images.Add(Image.FromStream(ms));
+                    //pictureBox1.Image = Image.FromStream(ms);
+                    //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
+            }
+            con.Close();
+        }
+    }
+}
